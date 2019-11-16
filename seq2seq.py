d83782e7f518e268fe93204121329df8853f8487
Rudhra Raveendran <rooday@bu.edu>
2019-11-16 18:45:05 -0500

    model doesnt break

--- seq2seq.py
+++ seq2seq.py
@@ -22,7 +22,8 @@ class Encoder(nn.Module):
     dropped = self.dropout(src)
     #print(dropped)
     #dropped = [src sent len, batch size]
-    #print("need: {}, got: {}".format(self.input_dim, dropped.shape)) 
+    dropped = dropped.view(1, dropped.shape[0], dropped.shape[1])
+    #print('encoder input shape: ', dropped.shape)
     outputs, (hidden, cell) = self.rnn(dropped)
     
     #outputs = [src sent len, batch size, hid dim * n directions]
@@ -41,7 +42,7 @@ class Decoder(nn.Module):
     self.hid_dim = hid_dim
     self.n_layers = n_layers
     
-    self.rnn = nn.LSTM(output_dim, hid_dim, n_layers, dropout = dropout)
+    self.rnn = nn.LSTM(output_dim, hid_dim, n_layers, dropout = dropout, batch_first=True)
     self.out = nn.Linear(hid_dim, output_dim)
     self.dropout = nn.Dropout(dropout)
       
@@ -54,7 +55,7 @@ class Decoder(nn.Module):
     #n directions in the decoder will both always be 1, therefore:
     #hidden = [n layers, batch size, hid dim]
     #context = [n layers, batch size, hid dim]
-    
+    #print('decoder input shape:', input.shape) 
     input = input.unsqueeze(0)
     
     #input = [1, batch size]
@@ -62,7 +63,9 @@ class Decoder(nn.Module):
     dropped = self.dropout(input)
     
     #dropped = [1, batch size]
-    dropped = dropped.view(len(input), len(input[0]), len(input[0][0]))         
+    #dropped = dropped.view(len(input), len(input[0]), len(input[0][0]))
+    dropped = dropped.view(-1).view(1,1,51) 
+    #print("decoder dropped shape:", dropped.shape)
     output, (hidden, cell) = self.rnn(dropped, (hidden, cell))
     
     #output = [sent len, batch size, hid dim * n directions]
@@ -73,9 +76,7 @@ class Decoder(nn.Module):
     #output = [1, batch size, hid dim]
     #hidden = [n layers, batch size, hid dim]
     #cell = [n layers, batch size, hid dim]
-    
-    prediction = self.out(output.squeeze(0))
-    
+    prediction = self.out(output.squeeze(0)).reshape((17,3))
     #prediction = [batch size, output dim]
     
     return prediction, hidden, cell
@@ -94,22 +95,21 @@ class Seq2Seq(nn.Module):
       "Encoder and decoder must have equal number of layers!"
       
   def forward(self, src, trg, teacher_forcing_ratio = 0.5):
-    
     #src = [src sent len, batch size]
     #trg = [trg sent len, batch size]
     #teacher_forcing_ratio is probability to use teacher forcing
     #e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
-    
+    #print('target shape:', trg.shape) 
     batch_size = trg.shape[1]
     max_len = trg.shape[0]
     trg_vocab_size = self.decoder.output_dim
     
     #tensor to store decoder outputs
-    outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+    outputs = torch.zeros(trg.shape).to(self.device)
     
     #last hidden state of the encoder is used as the initial hidden state of the decoder
     hidden, cell = self.encoder(src)
-    
+    #print('encoder output shape:', hidden.shape)
     #first input to the decoder is the <sos> tokens
     input = trg[0,:]
     
@@ -117,8 +117,9 @@ class Seq2Seq(nn.Module):
       
       #insert input token embedding, previous hidden and previous cell states
       #receive output tensor (predictions) and new hidden and cell states
+      #print('seqseq input:', input)
       output, hidden, cell = self.decoder(input, hidden, cell)
-      
+      #print('seqseq output:', output)
       #place predictions in a tensor holding predictions for each token
       outputs[t] = output
       
@@ -130,7 +131,7 @@ class Seq2Seq(nn.Module):
       
       #if teacher forcing, use actual next token as next input
       #if not, use predicted token
-      input = trg[t] if teacher_force else top1
+      input = trg[t] if teacher_force else output
     
     return outputs
 
@@ -148,22 +149,25 @@ def train(model, iterator, optimizer, criterion, clip):
     trg = batch['trg']
     
     optimizer.zero_grad()
-    
+   
+    print('\n=> Predicting output...') 
     output = model(src, trg)
-    
     #trg = [trg sent len, batch size]
     #output = [trg sent len, batch size, output dim]
     
-    output = output[1:].view(-1, output.shape[-1])
-    trg = trg[1:].view(-1)
+    output = output[1:-1]#.view(-1, output.shape[-1])
+    output = output.reshape(output.shape[0], model.decoder.output_dim)
+    trg = trg[1:-1]#.view(-1)
+    trg = trg.reshape(trg.shape[0], model.decoder.output_dim)
+    #print("pred: {}, trg: {}".format(output.shape, trg.shape))
     
     #trg = [(trg sent len - 1) * batch size]
     #output = [(trg sent len - 1) * batch size, output dim]
-    
+    print('=> Calculating loss...')
     loss = criterion(output, trg)
-    
+    print('=> Backpropagating...')
     loss.backward()
-    
+    print('=> Clipping gradients...')
     torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
     
     optimizer.step()
@@ -182,17 +186,20 @@ def evaluate(model, iterator, criterion):
       src = batch['src']
       trg = batch['trg']
 
+      print('\n=> Predicting output...')
       output = model(src, trg, 0) #turn off teacher forcing
 
       #trg = [trg sent len, batch size]
       #output = [trg sent len, batch size, output dim]
 
-      output = output[1:].view(-1, output.shape[-1])
-      trg = trg[1:].view(-1)
+      output = output[1:-1]#.view(-1, output.shape[-1])
+      output = output.reshape(output.shape[0], model.decoder.output_dim)
+      trg = trg[1:-1]#.view(-1)
+      trg = trg.reshape(trg.shape[0], model.decoder.output_dim)
 
       #trg = [(trg sent len - 1) * batch size]
       #output = [(trg sent len - 1) * batch size, output dim]
-
+      print('=> Calculating loss...')
       loss = criterion(output, trg)
       
       epoch_loss += loss.item()
@@ -224,24 +231,24 @@ keypoints = [np.pad(kp, [(max_kp_len-len(kp), 0), (0,0), (0,0)]) for kp in keypo
 
 np.insert(mfccs[0], 0, np.full((20,), -1), axis=0).shape
 
-input_sos = np.full((20,), -1)
-input_eos = np.full((1,20), np.inf)
-output_sos = np.full((1, 17, 3), -1)
-output_eos = np.full((1, 17, 3), np.inf)
+input_sos = np.full((20,), -0.01)
+input_eos = np.full((1,20), 0.01)
+output_sos = np.full((1, 17, 3), -0.01)
+output_eos = np.full((1, 17, 3), 0.01)
 
-#it = [{ 'src': torch.tensor(np.append(np.insert(mfcc, 0, np.zeros((20,)), axis=0), input_eos, axis=0)), 'trg': torch.tensor(np.append(np.insert(kp, 0, output_sos, axis=0), output_eos, axis=0))} for mfcc, kp in zip(mfccs, keypoints)]
+it = [{ 'src': torch.tensor(np.append(np.insert(mfcc, 0, input_sos, axis=0), input_eos, axis=0)).float(), 'trg': torch.tensor(np.append(np.insert(kp, 0, output_sos, axis=0), output_eos, axis=0)).float()} for mfcc, kp in zip(mfccs, keypoints)]
 
 batch_mfccs = torch.tensor([np.append(np.insert(mfcc, 0, np.zeros((20,)), axis=0), input_eos, axis=0) for mfcc in mfccs]).float()
 batch_kps   = torch.tensor([np.append(np.insert(kp, 0, output_sos, axis=0), output_eos, axis=0) for kp in keypoints]).float()
-it = [{'src': batch_mfccs, 'trg': batch_kps}]
+#it = [{'src': batch_mfccs, 'trg': batch_kps}]
 
-for x in it:
+#for x in it:
  # print(x) 
- print('src shape: {}, trg shape: {}'.format(x['src'].shape, x['trg'].shape))
+ #print('src shape: {}, trg shape: {}'.format(x['src'].shape, x['trg'].shape))
 #exit()
 
 INPUT_DIM = 20
-OUTPUT_DIM = 17
+OUTPUT_DIM = 51
 HID_DIM = 512
 N_LAYERS = 2
 ENC_DROPOUT = 0.5
@@ -254,7 +261,7 @@ model = Seq2Seq(enc, dec, device).to(device)
 model.apply(init_weights)
 
 optimizer = optim.Adam(model.parameters())
-criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 
 N_EPOCHS = 10
 CLIP = 1
@@ -264,7 +271,9 @@ best_valid_loss = float('inf')
 for epoch in range(N_EPOCHS):  
   start_time = time.time()
   
+  print('=> Training epoch {}\n========'.format(epoch))
   train_loss = train(model, it, optimizer, criterion, CLIP)
+  print('=> Evaluating epoch {}\n========'.format(epoch))
   valid_loss = evaluate(model, it, criterion)
   
   end_time = time.time()
