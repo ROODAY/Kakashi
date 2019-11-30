b89b546401f3bbc1ba4505e51bfe47c9df311f20
Rudhra Raveendran <rooday@bu.edu>
2019-11-29 20:58:53 -0500

    fixed infer

--- infer.py
+++ infer.py
@@ -1,4 +1,4 @@
-from models.seq2seq import Seq2Seq
+from models.lstm import Encoder, Decoder, Seq2Seq
 from pathlib import Path
 import torch
 import numpy as np
@@ -15,7 +15,6 @@ def main(args):
   N_LAYERS = 2
   ENC_DROPOUT = 0.5
   DEC_DROPOUT = 0.5
-  MODEL_NAME = 'kakashi-{}'.format(args.label)
 
   enc = Encoder(INPUT_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT)
   dec = Decoder(OUTPUT_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT)
@@ -26,21 +25,24 @@ def main(args):
   print('=> Processing Input')
   input_path = Path(args.input_path)
   audio_data, sample_rate = librosa.load(input_path, res_type='kaiser_fast')
-  frames = round(librosa.get_duration(y=audio_data, sr=sample_rate) * 30)
+  frames = round(librosa.get_duration(y=audio_data, sr=sample_rate) * 24)
   grouped_audio = np.array_split(audio_data, frames)
   features = np.array([np.mean(librosa.feature.mfcc(y=group).T,axis=0) for group in grouped_audio])
   features = features.reshape(features.shape[0], 1, features.shape[1])
+  inp = torch.tensor(features).float().to(device)
 
-  print('=> Running Model')
-  output = model(feature, None, 0, True)
+  with torch.no_grad():
+    print('=> Running Model')
+    output = model(inp, None, 0, True)
+    output_dir = Path(Path.cwd(), 'out/infer')
+    output_dir.mkdir(exist_ok=True, parents=True)
+    output_path = Path(output_dir, '{}.keypoints.npy'.format(input_path.stem))
 
-  output_path = Path(Path.cwd(), 'out/infer/{}.keypoints.npy'.format(input_path.stem))
-  print('=> Saving output to {}'.format(output_path))
-  seq_len, batch_size, _ = output.shape
-  unrolled_features = output.reshape(seq_len, batch_size, 17, 3)
-  batch_first = torch.transpose(unrolled_features, 0, 1)
-  keypoints = batch_first.cpu().numpy()
-  np.save(output_path, keypoints)
+    print('=> Saving output to {}'.format(output_path))
+    seq_len, _, _ = output.shape
+    unrolled_features = output.reshape(seq_len, 17, 3)
+    keypoints = unrolled_features.cpu().numpy()
+    np.save(output_path, keypoints)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Infer choreography from audio file')
