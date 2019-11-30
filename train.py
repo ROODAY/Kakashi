4f39496481ddd2667f020a8b04ed4f2adb123844
Rudhra Raveendran <rooday@bu.edu>
2019-11-30 17:44:14 -0500

    switched loss

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ def main(args):
   model.apply(init_weights)
 
   optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-  criterion = MAPELoss#MSE_Diff#nn.SmoothL1Loss()#MSELoss()
+  criterion = RPDLoss#MSE_Diff#nn.SmoothL1Loss()#MSELoss()
 
   output_dir = Path(Path.cwd(),'out/{}'.format(args.label))
   if output_dir.exists():
