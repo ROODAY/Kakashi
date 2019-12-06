f26cb8123bec2cc68b5c163ca0a3e7082a7acaa0
Rudhra Raveendran <rudytheninja@gmail.com>
2019-12-05 19:15:34 -0500

    allow inference with extracted features

--- infer.py
+++ infer.py
@@ -32,15 +32,22 @@ def main(args):
   model.load_state_dict(torch.load(model_path))
 
   print('=> Processing Input')
+  # Load input, and if necessary extract features
   input_path = Path(args.input_path)
-  audio_data, sample_rate = librosa.load(input_path, res_type='kaiser_fast')
-  frames = round(librosa.get_duration(y=audio_data, sr=sample_rate) * FRAME_RATE)
-  grouped_audio = np.array_split(audio_data, frames)
-  features = [np.mean(librosa.feature.mfcc(y=group).T,axis=0) for group in grouped_audio]
+  if args.raw_features:
+    features = np.load(input_path)
+  else:
+    audio_data, sample_rate = librosa.load(input_path, res_type='kaiser_fast')
+    frames = round(librosa.get_duration(y=audio_data, sr=sample_rate) * FRAME_RATE)
+    grouped_audio = np.array_split(audio_data, frames)
+    features = [np.mean(librosa.feature.mfcc(y=group).T,axis=0) for group in grouped_audio]
+
+  # Convert features to float tensor, reshape to (seq_len, batch_size=1, feature_len), and push to device
   features = torch.tensor(features).float()
   features = features.reshape(features.shape[0], 1, features.shape[1]).to(device)
 
   print('=> Selecting Seed Pose')
+  # Pick a pose to start the dance
   all_poses = list(Path(Path.cwd(), 'data/{}'.format(args.seed_label)).rglob('*.keypoints.npy'))
   seed_pose = torch.tensor(np.load(random.choice(all_poses))[:1])
 
@@ -54,23 +61,27 @@ def main(args):
 
     print('=> Saving output to {}'.format(output_path))
     seq_len, _, _ = output.shape
+    # Remove batch dimension before saving so we can render if desired
     unrolled_features = output.reshape(seq_len, 17, 3)
     keypoints = unrolled_features.cpu().numpy()
     np.save(output_path, keypoints)
 
   if args.render:
     print('=> Calling Renderer')
+    # Use custom VideoPose script to render keypoints
     os.chdir(os.environ['VIDEOPOSE'])
     video_path = Path(output_dir, '{}.mp4'.format(input_path.stem))
     command = 'python3 animate.py --viz-input {} --viz-output {}'.format(output_path, video_path)
     subprocess.call(command, shell=True)
 
-    print('=> Muxing')
-    os.chdir(os.environ['KAKASHI'])
-    tmp_path = Path(output_dir, 'output.mp4')
-    command = 'ffmpeg -i {} -i {} -c:v copy -c:a aac -strict experimental {}'.format(video_path, input_path, tmp_path)
-    subprocess.call(command, shell=True)
-    tmp_path.replace(video_path)
+    if not args.raw_features:
+      print('=> Muxing')
+      # Mux rendered video with original audio
+      os.chdir(os.environ['KAKASHI'])
+      tmp_path = Path(output_dir, 'output.mp4')
+      command = 'ffmpeg -i {} -i {} -c:v copy -c:a aac -strict experimental {}'.format(video_path, input_path, tmp_path)
+      subprocess.call(command, shell=True)
+      tmp_path.replace(video_path)
 
     print('=> Final output stored at {}'.format(video_path))
 
@@ -86,5 +97,7 @@ if __name__ == "__main__":
                       help='Dataset label to grab seed frame from')
   parser.add_argument('--render', action='store_true',
                       help='Render saved keypoints')
+  parser.add_argument('--raw_features', action='store_true',
+                      help='Input file being passed is already a set of features')
   args = parser.parse_args()
   main(args)
